8b4b2e178df92a962b3dbf9f1acccdc82dcd1d90
Andrew Chi <achi@cs.unc.edu>
2016-02-16 13:09:16 -0500

    [1.0.1ef] PKCS12_create(): apply shortcut logic

--- crypto/pkcs12/p12_crt.c
+++ crypto/pkcs12/p12_crt.c
@@ -90,7 +90,16 @@ PKCS12 *PKCS12_create(char *pass, char *name, EVP_PKEY *pkey, X509 *cert,
 
 	/* Set defaults */
 	if (!nid_cert)
+  {
+#ifdef OPENSSL_FIPS
+#ifdef CLIVER
+  if (FIPS_mode() && composed_version == COMPOSED_F)
+    nid_cert = NID_pbe_WithSHA1And3_Key_TripleDES_CBC;
+  else
+#endif // CLIVER
+#endif // OPENSSL_FIPS
 		nid_cert = NID_pbe_WithSHA1And40BitRC2_CBC;
+  }
 	if (!nid_key)
 		nid_key = NID_pbe_WithSHA1And3_Key_TripleDES_CBC;
 	if (!iter)
