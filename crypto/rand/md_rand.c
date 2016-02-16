dd5517c0c8688615ae70fc905a33b8c908e8da16
Andrew Chi <achi@cs.unc.edu>
2016-02-16 13:27:18 -0500

    [1.0.1ef] start merging crypto/rand directory
    Often this is FIPS related, and I use shortcut logic since it's not too
    complicated.

--- crypto/rand/md_rand.c
+++ crypto/rand/md_rand.c
@@ -133,6 +133,10 @@
 # define PREDICT
 #endif
 
+#ifdef CLIVER
+#include <openssl/KTest.h>
+#endif
+
 /* #define PREDICT	1 */
 
 #define STATE_SIZE	1023
@@ -380,7 +384,12 @@ static int ssleay_rand_bytes(unsigned char *buf, int num, int pseudo)
 	 * are fed into the hash function and the results are kept in the
 	 * global 'md'.
 	 */
-
+#ifdef OPENSSL_FIPS
+#ifdef CLIVER
+	/* NB: in FIPS mode we are already under a lock */
+	if (!FIPS_mode() || composed_version == COMPOSED_E)
+#endif
+#endif
 	CRYPTO_w_lock(CRYPTO_LOCK_RAND);
 
 	/* prevent ssleay_rand_bytes() from trying to obtain the lock again */
@@ -460,6 +469,11 @@ static int ssleay_rand_bytes(unsigned char *buf, int num, int pseudo)
 
 	/* before unlocking, we must clear 'crypto_lock_rand' */
 	crypto_lock_rand = 0;
+#ifdef OPENSSL_FIPS
+#ifdef CLIVER
+	if (!FIPS_mode() || composed_version == COMPOSED_E)
+#endif
+#endif
 	CRYPTO_w_unlock(CRYPTO_LOCK_RAND);
 
 	while (num > 0)
@@ -512,9 +526,19 @@ static int ssleay_rand_bytes(unsigned char *buf, int num, int pseudo)
 	MD_Init(&m);
 	MD_Update(&m,(unsigned char *)&(md_c[0]),sizeof(md_c));
 	MD_Update(&m,local_md,MD_DIGEST_LENGTH);
-	CRYPTO_w_lock(CRYPTO_LOCK_RAND);
+#ifdef OPENSSL_FIPS
+#ifdef CLIVER
+	if (!FIPS_mode() || composed_version == COMPOSED_E)
+#endif
+#endif
+  CRYPTO_w_lock(CRYPTO_LOCK_RAND);
 	MD_Update(&m,md,MD_DIGEST_LENGTH);
 	MD_Final(&m,md);
+#ifdef OPENSSL_FIPS
+#ifdef CLIVER
+	if (!FIPS_mode() || composed_version == COMPOSED_E)
+#endif
+#endif
 	CRYPTO_w_unlock(CRYPTO_LOCK_RAND);
 
 	EVP_MD_CTX_cleanup(&m);
