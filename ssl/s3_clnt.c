3f258de5b51eda0d186dc66b43f9a0e289172f18
Marie Nesfield <nesfield@cs.unc.edu>
2016-02-16 15:54:15 -0500

    TIME HACK, set Time to 0 for first 4 bytes of client random for trace collection

--- ssl/s3_clnt.c
+++ ssl/s3_clnt.c
@@ -685,7 +685,8 @@ int ssl3_client_hello(SSL *s)
             if (ssl_fill_hello_random(s, 0, p, SSL3_RANDOM_SIZE) <= 0)
                 goto err;
         } else if (composed_version == COMPOSED_E){
-            Time=(unsigned long)time(NULL);			/* Time */
+            time_t t = 0; //side note: time_t     time(time_t *);
+            Time=(unsigned long) t;//time(NULL);			/* Time */
 		    l2n(Time,p);
 		    if (RAND_pseudo_bytes(p,SSL3_RANDOM_SIZE-4) <= 0)
 			    goto err;
