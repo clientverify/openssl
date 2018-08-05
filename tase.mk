61032a84a8fb19efb638caf4e1a953f0d9f849b8
Kartik Null Cating-Subramanian <kncs@cs.unc.edu>
2018-08-05 16:51:39 -0400

    Make rules to build TASE to be injected into Makefiles

---
+++ tase.mk
@@ -0,0 +1,37 @@
+TASE_PLUGIN_PATH=$(abspath $(dir $(CC))../lib/LLVMTase.so)
+OPT=$(dir $(CC))opt
+LLC=$(dir $(CC))llc
+DISAS=$(dir $(CC))llvm-dis
+LLC=$(dir $(CC))/llc
+OBJDUMP=objdump -d -w -M suffix -j .text
+
+TASE_CORE=$(abspath $(TOP))/core_instrumented
+TASE_MODELED=$(abspath $(TOP))/core_modeled
+TASE_MODELED_FLAGS=-tase-modeled-functions $(TASE_MODELED)
+PROJECT_TASE_ALL=$(abspath $(TOP))/openssl.tase
+LLC_FLAGS=-O2
+
+DEF_MODE_PERF         = -DNOVERIFY
+DEF_MODE_REFERENCE    = -DNOTSX -DNOVERIFY
+DEF_MODE_VERIFY_DUMMY = -DVERIFY_DUMMY
+DEF_MODE=$(DEF_MODE_PERF)
+EXTCPPFLAGS=$(CPPFLAGS) $(TASE_INCLUDES) $(DEF_MODE)
+
+#$(PROJECT_TASE_ALL): $(PROJECT_TASES) $(TASE_CORE)
+#	cat $^ | sort | uniq > $@
+
+
+$(LIBOBJ): %.o: %.bc | $(PROJECT_TASE_ALL)
+	$(LLC) $(LLC_FLAGS) -tase-instrumented-functions $(PROJECT_TASE_ALL) $(TASE_MODELED_FLAGS) -o $*.s $<
+	$(CC) $(CFLAGS) $(EXTCPPFLAGS) $(DEF_MODE) -O2 -o $@ -c $*.s
+
+tasescan: $(LIBOBJ:.o=.bc)
+	@target=tasescan; $(RECURSIVE_MAKE)
+
+$(LIBOBJ:.o=.bc): %.bc: %.c
+	$(CC) $(CFLAGS) $(EXTCPPFLAGS) $(DEF_MODE) -emit-llvm -o $*.init.bc -c $<
+	$(DISAS) $*.init.bc
+	$(OPT) -load $(TASE_PLUGIN_PATH) -function-wrapper -tase-instrumented-functions $*.tase $(TASE_MODELED_FLAGS) -o $@ $*.init.bc
+	$(DISAS) $*.bc
+
+
